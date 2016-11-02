7201d6cdaaef78be035893307eae6ec531e36732
Anthony Ccapira A <anthony.ccapira@ucsp.edu.pe>
2016-11-01 22:28:25 -0500

    Molecular, fixing some problems,.

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ class Upgma  {
     public:
       std::size_t row, col;
       float ** matriz;
+      float value;
       vector<string> header;
       string file;
 
@@ -40,14 +41,14 @@ class Upgma  {
           size_t row, size_t col) {
 
           int positions[2] = {1, 0};
-          double value = matriz[1][0];
+          this->value = matriz[1][0];
           bool flag = false;
           for (int x = 1; x < row; ++x) {
             for (int y = 0; y < x; ++y) {
-                if ( flag && matriz[x][y] < value ) {
+                if ( flag && matriz[x][y] < this->value ) {
                   positions[0] = x;
                   positions[1] = y;
-                  value = matriz[x][y];
+                  this->value = matriz[x][y];
                 }
                 else
                   flag = true;
@@ -56,17 +57,19 @@ class Upgma  {
           vector<int> pos;
           for (int u=0; u<2; ++u)
               pos.push_back(positions[u]);
-          cout << "\n" << setw(6) << "Min: " << value << ", position("<<pos.at(0) << 
+          cout << "\n" << setw(6) << "Min: " << this->value << ", position("<<pos.at(0) << 
               ", " <<pos.at(1)<< "), new: " << header[pos.at(0)] << header[pos.at(1)];
           return pos;
       }
 
       void getCluster() {
+          std::ofstream out("tree.txt");
           cout << "\n Original Matriz: \n";
           printMatriz(this->matriz, this->header, row, col);
           int clusters = row;
           float ** currentMatriz = this->matriz;
           float ** dynamic_matriz;
+          string result;
           vector<string> dynamic_header = this->header;
           while(clusters > 2) {
               cout << "\n **************************************** \n";
@@ -80,6 +83,9 @@ class Upgma  {
               temporal_header.push_back(
                   dynamic_header.at(
                     min_position.at(0)) + dynamic_header.at(min_position.at(1)));
+              result = "("+dynamic_header.at(min_position.at(0))
+                + "," + dynamic_header.at(min_position.at(1))+"):"+std::to_string(this->value);
+              out << result << endl;
               clusters --;        
               dynamic_matriz = createMatriz(clusters, clusters);
               size_t temp_x = 0;
@@ -88,7 +94,7 @@ class Upgma  {
               for (size_t x=0; x<clusters+1; ++x) {
                   size_t temp_y = 0;
                   if (x!=min_position.at(0) && x!=min_position.at(1)) {
-                    for (size_t y=0; y<clusters; ++y) {
+                    for (size_t y=0; y<clusters+1; ++y) {
                         if (y!=min_position.at(0) && y!=min_position.at(1)) {
                             dynamic_matriz[temp_x][temp_y] = currentMatriz[x][y];
                             temp_y++;
@@ -103,7 +109,11 @@ class Upgma  {
                       sum += currentMatriz[0][u];
                   }
                   dynamic_matriz[1][0] = sum/2;
-                  dynamic_matriz[0][1] = dynamic_matriz[1][0];                  
+                  dynamic_matriz[0][1] = dynamic_matriz[1][0];
+                  string temp = "(("+temporal_header.at(0)
+                      + "," + temporal_header.at(1)+"):"+std::to_string(dynamic_matriz[1][0])
+                      + ");";
+                  out <<  temp;                 
               }
               else {
                   size_t u = 0;
@@ -122,6 +132,7 @@ class Upgma  {
               currentMatriz = dynamic_matriz;
               dynamic_header = temporal_header;
           }
+          out.close();
       }
 
       void LoadMatriz() {
